b31ac6e38b564b897b80f26a30dbdda340b550c8
yunb12345 <agusyoon11@hotmail.com>
2025-05-07 21:13:16 -0300

    conexion con mongo

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 import logging
 
+#para testear conexion con mongo
+from app.database import get_test_collection
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     logging.info("Flask app started")
+
+    """
+    test = get_test_collection()
+    documentos = list(test.find())
+    for doc in documentos:
+        print(doc)
+    """
     app.run(host="0.0.0.0", port=8000)
